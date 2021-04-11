c90d0edd2f1a0306c03d4b4a497662a456b1e20b
Henry Larson <hxl1116@rit.edu>
2021-04-11 13:51:46 -0400

    Initial commit

---
+++ main.py
@@ -0,0 +1,21 @@
+from discord import Client
+
+client = Client()
+
+
+@client.event
+def on_ready():
+    pass
+
+
+@client.event
+def on_message():
+    pass
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
